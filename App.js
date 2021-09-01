ebf1444798630359ddc8abae2dc39867e62584ab
Gapur Kassym <gapur.kassym@gmail.com>
2021-09-01 13:52:01 +0600

    feat(app): implement modal with gallery and camera buttons

--- App.js
+++ App.js
@@ -7,21 +7,38 @@ import {
   TouchableOpacity,
   Text,
   ImageBackground,
+  Pressable,
 } from 'react-native';
 import * as ImagePicker from 'react-native-image-picker';
+import Modal from 'react-native-modal';
 
 import {images} from './assets';
 
 export default function App() {
   const [response, setResponse] = React.useState(null);
+  const [visible, setVisible] = React.useState(false);
 
-  const onButtonPress = React.useCallback((type, options) => {
-    if (type === 'capture') {
-      ImagePicker.launchCamera(options, setResponse);
-    } else {
-      ImagePicker.launchImageLibrary(options, setResponse);
-    }
+  const onImageLibraryPress = React.useCallback(() => {
+    const options = {
+      maxHeight: 200,
+      maxWidth: 200,
+      selectionLimit: 1,
+      mediaType: 'photo',
+      includeBase64: false,
+    };
+    ImagePicker.launchImageLibrary(options, setResponse);
   }, []);
+
+  const onCameraPress = React.useCallback(() => {
+    const options = {
+      saveToPhotos: true,
+      mediaType: 'photo',
+      includeBase64: false,
+    };
+    ImagePicker.launchCamera(options, setResponse);
+  }, []);
+
+  const uri = response?.assets && response.assets[0].uri;
 
   return (
     <View style={styles.screen}>
@@ -34,13 +51,34 @@ export default function App() {
         style={styles.imageBackground}
         source={images.whatsappBackground}>
         <View style={styles.avatar}>
-          <Image style={styles.avatarImage} source={images.avatar} />
-          <TouchableOpacity style={styles.addButton}>
+          <Image
+            style={styles.avatarImage}
+            source={uri ? {uri} : images.avatar}
+          />
+          <TouchableOpacity
+            style={styles.addButton}
+            onPress={() => setVisible(true)}>
             <Image style={styles.addButtonIcon} source={images.addButton} />
           </TouchableOpacity>
           <Text style={styles.usernameText}>Gapur Kassym</Text>
         </View>
       </ImageBackground>
+      <Modal
+        isVisible={visible}
+        onBackButtonPress={() => setVisible(false)}
+        onBackdropPress={() => setVisible(false)}
+        style={styles.modal}>
+        <SafeAreaView style={styles.options}>
+          <Pressable style={styles.option} onPress={onImageLibraryPress}>
+            <Image style={styles.icon} source={images.image} />
+            <Text>Library</Text>
+          </Pressable>
+          <Pressable style={styles.option} onPress={onCameraPress}>
+            <Image style={styles.icon} source={images.camera} />
+            <Text>Camera</Text>
+          </Pressable>
+        </SafeAreaView>
+      </Modal>
     </View>
   );
 }
@@ -96,5 +134,25 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: '#ffffff',
     marginTop: 12,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    margin: 10,
+  },
+  options: {
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  option: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0,
   },
 });
--- assets/index.js
+++ assets/index.js
@@ -2,4 +2,6 @@ export const images = {
   addButton: require('./add-button.png'),
   avatar: require('./avatar.jpg'),
   whatsappBackground: require('./whatsapp-background.jpg'),
+  image: require('./image.png'),
+  camera: require('./camera.png'),
 };
